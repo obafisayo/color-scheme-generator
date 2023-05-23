e653082552309da7f40ab7d7908e8dcc3ccb670d
obafisayo <125868532+obafisayo@users.noreply.github.com>
2023-05-23 19:11:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,30 @@ getColorSchemes.addEventListener("click", function() {
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${colorMode.value}`)
     .then(res => res.json())
     .then(data => {
+        console.log(data.colors[0].name.value)
         footer.innerHTML = `
-                <h5>${data.colors[0].hex.value}</h5>
-                <h5>${data.colors[1].hex.value}</h5>
-                <h5>${data.colors[2].hex.value}</h5>
-                <h5>${data.colors[3].hex.value}</h5>
-                <h5>${data.colors[4].hex.value}</h5>
+                <h5>${data.colors[0].hex.value} <br>
+                    ${data.colors[0].name.value}
+                </h5>
+                <h5>${data.colors[1].hex.value} <br>
+                    ${data.colors[1].name.value}
+                </h5>
+                <h5>${data.colors[2].hex.value} <br>
+                    ${data.colors[2].name.value}
+                </h5>
+                <h5>${data.colors[3].hex.value} <br>
+                    ${data.colors[3].name.value}
+                </h5>
+                <h5>${data.colors[4].hex.value} <br>
+                    ${data.colors[4].name.value}
+                </h5>
         `
         schemes.innerHTML = `
-            <img id="Color" src="${data.colors[0].image.named}" />
-            <img id="Color" src="${data.colors[1].image.named}" />
-            <img id="Color" src="${data.colors[2].image.named}" />
-            <img id="Color" src="${data.colors[3].image.named}" />
-            <img id="Color" src="${data.colors[4].image.named}" />
+            <img id="Color" src="${data.colors[0].image.bare}" />
+            <img id="Color" src="${data.colors[1].image.bare}" />
+            <img id="Color" src="${data.colors[2].image.bare}" />
+            <img id="Color" src="${data.colors[3].image.bare}" />
+            <img id="Color" src="${data.colors[4].image.bare}" />
         `
     })
 })
